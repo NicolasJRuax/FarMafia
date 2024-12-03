5ccaa5ad6dac8f81a5d87c731369da9af19f1e8d
NicolasJRuax <160480830+NicolasJRuax@users.noreply.github.com>
2024-12-03 18:10:07 +0100

    Icono

--- app/src/main/java/com/myproyect/farmafia/PharmacyDetailActivity.java
+++ app/src/main/java/com/myproyect/farmafia/PharmacyDetailActivity.java
@@ -1,7 +1,9 @@
 package com.myproyect.farmafia;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +15,19 @@ public class PharmacyDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pharmacy_detail);
 
         WebView webView = findViewById(R.id.webView);
+
+        // Configuración del WebView
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Habilitar JavaScript
+        webView.setWebViewClient(new WebViewClient()); // Evitar abrir en navegador externo
+
+        // Obtener datos del Intent
         String name = getIntent().getStringExtra("name");
         double latitude = getIntent().getDoubleExtra("latitude", 0.0);
         double longitude = getIntent().getDoubleExtra("longitude", 0.0);
 
-        String url = "https://www.openstreetmap.org/?mlat=" + latitude + "&mlon=" + longitude + "#map=18/" + latitude + "/" + longitude;
+        // Construir URL y cargar en el WebView
+        String url = "https://www.openstreetmap.org/#map=18/" + latitude + "/" + longitude;
         webView.loadUrl(url);
     }
 }
